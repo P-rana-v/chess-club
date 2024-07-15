cdc9146d22c197bd57dd76dd2a92b0c0a35e38f4
Vishnu T C <vishnutc13@gmail.com>
2024-07-15 17:54:13 +0530

    feat: daily chess puzzle at page top

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,29 +1,47 @@
 import React from "react";
+import "./mystyles.css";
+import { useState, useEffect, useRef } from "react";
+import { Chessboard } from "react-chessboard";
+import { Chess } from "chess.js";
 
 export const Header = (props) => {
+  const [fen, setFen] = useState("");
+
+  const fetchDailyPuzzle = async() => {
+    try{
+        const response = await fetch("https://lichess.org/api/puzzle/daily");
+        const data = await response.json(); 
+        const chess = new Chess();
+        chess.loadPgn(data.game.pgn);
+        setFen(chess.fen());
+
+    } catch (error ){
+        console.log(error);
+    }  
+  }
+  useEffect(() => {
+    fetchDailyPuzzle()
+  }, []);
+
   return (
     <header id="header">
-      <div className="intro">
-        <div className="overlay">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-8 col-md-offset-2 intro-text">
-                <h1>
-                  {props.data ? props.data.title : "Loading"}
-                  <span></span>
-                </h1>
-                <p>{props.data ? props.data.paragraph : "Loading"}</p>
-                <a
-                  href="#features"
-                  className="btn btn-custom btn-lg page-scroll"
-                >
-                  Learn More
-                </a>{" "}
-              </div>
+          <div className="chess-start">
+            <div className="puzzle">
+              <a className="gotopuzzle" href="https://lichess.org/training/daily" 
+              target="_blank" rel="noreferrer">
+                <span>Daily Puzzle</span>
+                <Chessboard position={fen} arePiecesDraggable={false}/>
+              </a>
             </div>
+
+            {props.data && 
+              <div className="chessClub">
+                <h1>{props.data.title}</h1>
+                <p>{props.data.paragraph}</p>
+                <a href="#features" >Learn More</a>
+              </div>
+            }
           </div>
-        </div>
-      </div>
     </header>
   );
 };
